f9f50c9c3cba10bfcd1bbe1ef99458bcc774935e
Ankit <ankitkunwar.2104@gmail.com>
2022-03-28 09:42:19 -0400

    Adding the project initial structure

---
+++ src/webSearchEngine/WebSearchEngine.java
@@ -0,0 +1,16 @@
+package webSearchEngine;
+
+import java.io.IOException;
+
+public class WebSearchEngine {
+	
+	
+	//start the implementation
+	
+	public static void main(String[] args) throws IOException 
+	{
+		
+		
+	}
+
+}
